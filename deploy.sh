59c67f147765a35ba38e1e5baa284fafc97d9653
Ayush Kumar <90111187+ayushkr03@users.noreply.github.com>
2023-06-13 20:15:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #pip install -r requirements.txt & uvicorn main:app --reload 
-pip install -r requirements.txt & streamlit run myapp.py
+pip install -r requirements.txt & streamlit run myapp.py --server.port 80 --server.address 0.0.0.0
