1ec41e47fe2df0dc968cd9c7245d09400dd2405b
Ayush Kumar <90111187+ayushkr03@users.noreply.github.com>
2023-06-13 13:14:37 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ pip install -r requirements.txt
 
 if [ $? -eq 0 ]
 then
-    uvicorn main:app --host=0.0.0.0 --port=8000 &
-    streamlit run myapp.py &
+    uvicorn main:app --reload &      #host=0.0.0.0 --port=8000 &
+    #streamlit run myapp.py &
 else
     echo "Failed to install dependencies"
 fi
