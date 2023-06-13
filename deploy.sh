983ac04c1139cd4fe6c50de5b7c1706bb5e2f081
Ayush Kumar <90111187+ayushkr03@users.noreply.github.com>
2023-06-13 19:11:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #pip install -r requirements.txt & uvicorn main:app --reload 
-pip install -r requirements.txt & gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind=0.0.0.0 & streamlit run myapp.py
+pip install -r requirements.txt & streamlit run myapp.py
