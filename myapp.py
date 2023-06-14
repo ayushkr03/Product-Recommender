bf32b4b8ddb6a92dff0d206fe7974ce80918a3bf
Ayush Kumar <90111187+ayushkr03@users.noreply.github.com>
2023-06-14 16:38:09 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,8 +1,8 @@
+import base64
 import streamlit as st
 import requests
 import PIL.Image
 import io
-
 
 product_id_options=['B0009U69UG',
  'B00FQN7LXK',
@@ -35,36 +35,98 @@ prod_category_options=['clothing carhartt sweatshirts',
  'accessories dress shirts',
  'casual button down shirts guess']
 
+st.set_page_config(page_title='Product Recommender', page_icon=':moon:', layout='wide')
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
 
+img1 = get_img_as_base64("mygif.gif")
+img2 = get_img_as_base64("mygif3.gif")
 
-st.set_page_config(page_title='Product Recommender', page_icon=':smiley:', layout='wide')
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img1}");
+background-size: cover;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+opacity:0.5
+color:black;
+}}
+
+[data-testid="stVerticalBlock"]{{
+max-width: 100rem;
+border-radius: 8px;
+padding:15px 15px 15px 15px;
+background: rgb(222 222 217 / 55%);
+color: rgb(0 0 0);
+}}
+
+[data-testid="stSidebar"] {{
+background-image: url("data:image/png;base64,{img2}");
+background-size: cover;
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title("Product Recommender")
+
+# Using object notation
+with st.sidebar.container():
+    st.header("About")
+    st.subheader("“Our product recommender system uses machine learning algorithms to predict and suggest items that users would be interested in purchasing. Powered by Azure, our system is designed to help users find the products they want quickly and easily.”")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+
+    st.subheader('Contact Information')
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown('<a href="mailto:ayushkumar.ae@gmail.com"><img src="https://img.icons8.com/material-outlined/40/000000/new-post.png"/></a>', unsafe_allow_html=True)
+
+    with col2:
+        st.markdown('<a href="https://github.com/ayushkr03"><img src="https://img.icons8.com/material-outlined/40/000000/github.png"/></a>', unsafe_allow_html=True)
+
+    with col3:
+        st.markdown('<a href="https://www.linkedin.com/in/ayushkumar03/"><img src="https://img.icons8.com/material-outlined/40/000000/linkedin.png"/></a>', unsafe_allow_html=True)
 
 st.subheader("Search by Product ID")
 col1, col2 = st.columns(2)
-product_id = col1.text_input("Enter a product id") 
+product_id = col1.text_input("**Enter a product id**") 
 
-product_id2 = col2.selectbox('Or Select a Product ID', [''] + product_id_options)
+product_id2 = col2.selectbox('**Or Select a Product ID**', [''] + product_id_options)
 if product_id or product_id2:
     if product_id2:
         product_id = product_id2
-    response = requests.get(f"https://product-recommender.azurewebsites.net/{product_id}")
+    response = requests.get(f"http://product-recommender.azurewebsites.net/{product_id}")
     data = response.json()
     if "Error" in data:
         st.error(data["Error"])
     else:
-        st.write(f"Recommendations for {product_id}:")
+        st.write(f"**Recommendations for** _:orange[{product_id}]_:")
         for item in data:
-            st.markdown(f"- {item['product_title']} ({item['similarity']})")
+            st.markdown(f"- {item['product_title']} ( {item['similarity']} )")
 
 
 st.subheader("Search by Product Title & Category")            
 col3, col4 = st.columns(2)
 # Get the product title and category from the user
 
-product_title = col3.selectbox('Select a Product Title', [''] + prod_title_options)
-category = col4.selectbox("Select a category", [''] + prod_category_options)
+product_title = col3.selectbox('**Select a Product Title**', [''] + prod_title_options)
+category = col4.selectbox("**Select a category**", [''] + prod_category_options)
 
 # Define a variable that holds the URL of the default or placeholder image
 default_image_url = "https://via.placeholder.com/200x200?text=No+Image"
@@ -74,35 +136,41 @@ if product_title and category:
     # Send a POST request to the FastAPI endpoint with the user input as JSON data
     response = requests.post(f"https://product-recommender.azurewebsites.net/{product_title}/{category}", json={"product_title": product_title, "category": category})
     data = response.json()
-    if "Error" in data:
-        st.error(data["Error"])
-    else:
-        st.write(f"Recommendations for {product_title} - {category}:")
-        for item in data:
-            # Check if the product_img_url field is present and valid
-            if "imgurl" in item and item["imgurl"]:
-                try:
-                    # Get the image data from the product_img_url field
-                    img_data = requests.get(item["imgurl"]).content
-                    # Convert the image data to a bytes object
-                    img_bytes = io.BytesIO(img_data)
-                    # Open the image from the bytes object
-                    img = PIL.Image.open(img_bytes)
-                except Exception as e:
-                    # If there is an exception, use the default or placeholder image instead
-                    #st.warning(f"Failed to load or display image:")
+
+    with st.container():
+        if "Error" in data:
+            st.error(data["Error"])
+        else:
+            st.write(f"**Recommendations for _:orange[{product_title}]_ - {category}:**")
+            for item in data:
+                # Check if the product_img_url field is present and valid
+                if "imgurl" in item and item["imgurl"]:
+                    try:
+                        # Get the image data from the product_img_url field
+                        img_data = requests.get(item["imgurl"]).content
+                        # Convert the image data to a bytes object
+                        img_bytes = io.BytesIO(img_data)
+                        # Open the image from the bytes object
+                        img = PIL.Image.open(img_bytes)
+                    except Exception as e:
+                        # If there is an exception, use the default or placeholder image instead
+                        #st.warning(f"Failed to load or display image:")
+                        img_data = requests.get(default_image_url).content
+                        img_bytes = io.BytesIO(img_data)
+                        img = PIL.Image.open(img_bytes)
+                else:
+                    # If there is no valid product_img_url field, use the default or placeholder image instead
+                    st.error(f"No valid image URL found for {item['product_title']}")
                     img_data = requests.get(default_image_url).content
                     img_bytes = io.BytesIO(img_data)
                     img = PIL.Image.open(img_bytes)
-            else:
-                # If there is no valid product_img_url field, use the default or placeholder image instead
-                st.error(f"No valid image URL found for {item['product_title']}")
-                img_data = requests.get(default_image_url).content
-                img_bytes = io.BytesIO(img_data)
-                img = PIL.Image.open(img_bytes)
-            # Display the image, title, and similarity score in a column layout
-            col1, col2 = st.columns(2)
-            with col1:
-                st.image(img, width=200)
-            with col2:
-                st.markdown(f"- {item['product_title']} ({item['similarity']})")
+                # Display the image, title, and similarity score in a column layout
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.image(img, width=150)
+                with col2:
+                    st.markdown(f"- Title - {item['product_title']}")
+                    st.markdown(f"- Rating - {item['rating']}")
+                    st.markdown(f"- Similarity - {item['similarity']}")           
+            st.success('**Successful Recommendation! Have a Nice Day**', icon="✅")
+               
