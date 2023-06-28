ad75ce766e6bb145b1ac731673ea4d5fa36405bf
Ayush Kumar <90111187+ayushkr03@users.noreply.github.com>
2023-06-29 00:51:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #pip install -r requirements.txt & uvicorn main:app --reload 
-pip install -r requirements.txt & gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind=0.0.0.0
+pip install -r requirements.txt --upgrade & gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind=0.0.0.0
