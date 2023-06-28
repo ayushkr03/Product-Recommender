95ee4729b7a9dab46c2944843121569936f2dcb7
Ayush Kumar <90111187+ayushkr03@users.noreply.github.com>
2023-06-28 23:11:26 +0530

    modified:   requirements.txt

--- myapp.py
+++ myapp.py
@@ -3,191 +3,163 @@ import streamlit as st
 import requests
 import PIL.Image
 import io
+import pickle
+import pandas as pd
+import numpy as np
+import bz2file as bz2
 
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, 'rb')
+    data = pickle.load(data)
+    return data
 
-product_id_options=['B0009U69UG',
- 'B00FQN7LXK',
- 'B008TZWRBI',
- 'B006XW8A56',
- 'B0009G8BNS',
- 'B00075ZYRW',
- 'B009FNMDXA',
- 'B0057QR3MK',
- 'B001YK4GNC',
- 'B005YP21XU']
-prod_title_options=["Carhartt Men's Heavyweight Crewneck Sweatshirt",
- "Bay Island Sportswear Men's Bring Me The Horizon &quot;Smooli&quot; Slim Fit T-Shirt",
- "The Walking Dead Warning Sign Men's T-Shirt",
- 'Lego Star Wars -- Dark Pieces T-Shirt, X-Large',
- 'Authentic Pigment Unisex-Adult 5.6 Oz. Pigment-Dyed &amp; Direct-Dyed Ringspun Pocket T-Shirt',
- "Russell Athletic Men's Basic Cotton Tee",
- 'Clever Girl -- Jurassic Park Adult T-Shirt',
- 'Dragon Ball Z Goku Top:Small (Small)',
- '24 Stainless Steel Metal Collar Stays for Dress Shirts',
- 'GUESS Campus Shirt']
-prod_category_options=['clothing carhartt sweatshirts',
- 't shirts',
- 'clothing men t shirts',
- 't shirts men',
- 't shirts',
- 'clothing active shirts tees big tall',
- 'novelty t shirts men',
- 't shirts men',
- 'accessories dress shirts',
- 'casual button down shirts guess']
+global df,combined_features,FASTAPI_URL
 
-#st.set_page_config(page_title='Product Recommender', page_icon=':smiley:', layout='wide')
+#FASTAPI_URL = "https://my-product-recommender.azurewebsites.net"
 
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+FASTAPI_URL = "http://localhost:8000"
 
+df = pd.read_pickle("embedded.pkl")
+#df = decompress_pickle('sentencetransformer2.pbz2')
 
-img1 = get_img_as_base64("mygif.gif")
-img2 = get_img_as_base64("mygif3.gif")
+brand_counts = df['brand_name'].value_counts().sort_values(ascending=False)
+brand_options = brand_counts.index.tolist()
 
+product_id_options=['B000NZW3J8','B000ULN4NO','B00008JP7C','B001KU60XA','B0000ANHST','B004IPRWRC','B0037TPECK','B0068VM5T4','B0085A43W8']
+
+st.set_page_config(page_title='Product Recommender',page_icon='🛒', layout='wide')
 
 page_bg_img = f"""
 <style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("data:image/png;base64,{img1}");
-background-size: cover;
-background-position: top left;
-background-repeat: no-repeat;
-background-attachment: local;
-background-color: rgba(249,240,220,0.81);
-}}
-
-[class="main css-uf99v8 e1g8pov65"]{{
-background-color: rgba(249,240,220,0.81);
-color: rgb(0 0 0);
-}}
-
-[class="block-container css-1y4p8pa e1g8pov64"]{{
-max-width: 70rem;
-border-radius: 25px;
-background: rgb(255,252,223,0.94);
-color: rgb(0 0 0);
-}}
-
-[data-testid="stHeader"] {{
-border-radius: 2px;
-background-color: rgba(37,35,31,0.81);
-}}
-
-[data-testid="stSidebar"] {{
-background-image: url("data:image/png;base64,{img2}");
-background-size: cover;
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: local;
-overflow: hidden;
-}}
-
-[class="css-10vh4h9 e1akgbir0"]{{
-background: rgb(0 0 0 / 72%);
-}}
-
 [data-testid="stVerticalBlock"]{{
-max-width: 50rem;
+max-width: 100rem;
 border-radius: 8px;
-background: rgb(251,242,207,0.18);;
-color: rgb(0 0 0);
+padding:15px 15px 15px 15px;
+background: rgb(0 0 0 / 0.9%);
 }}
-
-[class="css-1544g2n e1akgbir4"]{{
-padding: 38rem 1rem 1rem;
+[data-testid="stHeader"]{{
+background: rgb(227 227 227 / 57%);
 }}
-
 </style>
 """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title("Product Recommender")
 
-# Using object notation
-with st.sidebar.container():
-    st.sidebar.subheader('Contact Information')
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.markdown('<a href="mailto:ayushkumar.ae@gmail.com"><img src="https://img.icons8.com/material-outlined/40/000000/new-post.png"/></a>', unsafe_allow_html=True)
+st.subheader("Search by Product ID")
+col1, col2, col3 = st.columns(3)
+product_id = col1.text_input("**Enter a product id**") 
 
-    with col2:
-        st.markdown('<a href="https://github.com/ayushkr03"><img src="https://img.icons8.com/material-outlined/40/000000/github.png"/></a>', unsafe_allow_html=True)
+product_id2 = col2.selectbox('**Or Select a Product ID**', [''] + product_id_options)
 
-    with col3:
-        st.markdown('<a href="https://www.linkedin.com/in/ayushkumar03/"><img src="https://img.icons8.com/material-outlined/40/000000/linkedin.png"/></a>', unsafe_allow_html=True)
+k = col3.slider('Select Number of Products to be recommended', 5, 20, 5, key="slider1")
 
-st.header("Search by Product ID")
-col1, col2 = st.columns(2)
-product_id = col1.text_input("Enter a product id") 
+default_image_url = "https://via.placeholder.com/200x200?text=No+Image"
 
-product_id2 = col2.selectbox('Or Select a Product ID', [''] + product_id_options)
 if product_id or product_id2:
     if product_id2:
         product_id = product_id2
-    response = requests.get(f"http://localhost:8000/{product_id}")
+    response = requests.get(f"{FASTAPI_URL}/{product_id}/{k}")
     data = response.json()
-    if "Error" in data:
-        st.error(data["Error"])
-    else:
-        st.write(f"Recommendations for {product_id}:")
-        for item in data:
-            st.markdown(f"- {item['product_title']} ( {item['similarity']} )")
-
-
-st.subheader("Search by Product Title & Category")            
-col3, col4 = st.columns(2)
+    
+    with st.container():
+        if "Error" in data:
+            st.error(data["Error"])
+        else:
+            st.write(f"**Recommendations for** _:orange[{product_id}]_:")
+            for item in data:
+                # Check if the product_img_url field is present and valid
+                if "imgurl" in item and item["imgurl"]:
+                    try:
+                        # Get the image data from the product_img_url field
+                        img_data = requests.get(item["imgurl"]).content
+                        # Convert the image data to a bytes object
+                        img_bytes = io.BytesIO(img_data)
+                        # Open the image from the bytes object
+                        img = PIL.Image.open(img_bytes)
+                    except Exception as e:
+                        # If there is an exception, use the default or placeholder image instead
+                        img_data = requests.get(default_image_url).content
+                        img_bytes = io.BytesIO(img_data)
+                        img = PIL.Image.open(img_bytes)
+                else:
+                    # If there is no valid product_img_url field, use the default or placeholder image instead
+                    img_data = requests.get(default_image_url).content
+                    img_bytes = io.BytesIO(img_data)
+                    img = PIL.Image.open(img_bytes)
+                # Display the image, title, and similarity score in a column layout
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.image(img, width=150, use_column_width ="auto")
+                with col2:
+                    st.markdown(f"- Product ID - {item['product_id']}")
+                    st.markdown(f"- Title - {item['product_title']}")
+                    st.markdown(f"- Brand - {item['brand_name']}")
+                    st.markdown(f"- Rating - {item['rating']}")
+                    st.markdown(f"- Category - {item['category']}") 
+                    st.markdown(f"- Review - {item['review_text'].replace('$', 'USD')}")
+                    st.markdown(f"- Similarity - {item['similarity']}")          
+            st.success('**Successful Recommendation!**', icon="✅")
+        
+st.subheader("Search by Brand & Category")            
+col3, col4, col5 = st.columns(3)
 # Get the product title and category from the user
 
-product_title = col3.selectbox('Select a Product Title', [''] + prod_title_options)
-category = col4.selectbox("Select a category", [''] + prod_category_options)
+brand_name = col3.selectbox('**Select a Brand**', [''] + brand_options)
+
+prod_category_options = list(set(df.loc[df['brand_name'] == brand_name]['category'].tolist()))
+
+category = col4.selectbox("**Select a category**", [''] + prod_category_options)
+
+k2 = col5.slider('Select Number of Products to be recommended', 5, 20, 5, key="slider2")
 
 # Define a variable that holds the URL of the default or placeholder image
 default_image_url = "https://via.placeholder.com/200x200?text=No+Image"
 
-if product_title and category:
+if brand_name and category:
 
     # Send a POST request to the FastAPI endpoint with the user input as JSON data
-    response = requests.post(f"http://localhost:8000/{product_title}/{category}", json={"product_title": product_title, "category": category})
+    response = requests.post(f"{FASTAPI_URL}/{brand_name}/{category}/{k2}", json={"brand_name": brand_name, "category": category})
     data = response.json()
-    if "Error" in data:
-        st.error(data["Error"])
-    else:
-        st.write(f"Recommendations for _:orange[{product_title}]_ - {category}:")
-        for item in data:
-            # Check if the product_img_url field is present and valid
-            if "imgurl" in item and item["imgurl"]:
-                try:
-                    # Get the image data from the product_img_url field
-                    img_data = requests.get(item["imgurl"]).content
-                    # Convert the image data to a bytes object
-                    img_bytes = io.BytesIO(img_data)
-                    # Open the image from the bytes object
-                    img = PIL.Image.open(img_bytes)
-                except Exception as e:
-                    # If there is an exception, use the default or placeholder image instead
-                    #st.warning(f"Failed to load or display image:")
+
+    with st.container():
+        if "Error" in data:
+            st.error(data["Error"])
+        else:
+            st.write(f"**Recommendations for Brand- _:orange[{brand_name}]_ and Category - {category}:**")
+            for item in data:
+                # Check if the product_img_url field is present and valid
+                if "imgurl" in item and item["imgurl"]:
+                    try:
+                        # Get the image data from the product_img_url field
+                        img_data = requests.get(item["imgurl"]).content
+                        # Convert the image data to a bytes object
+                        img_bytes = io.BytesIO(img_data)
+                        # Open the image from the bytes object
+                        img = PIL.Image.open(img_bytes)
+                    except Exception as e:
+                        # If there is an exception, use the default or placeholder image instead
+                        img_data = requests.get(default_image_url).content
+                        img_bytes = io.BytesIO(img_data)
+                        img = PIL.Image.open(img_bytes)
+                else:
+                    # If there is no valid product_img_url field, use the default or placeholder image instead
                     img_data = requests.get(default_image_url).content
                     img_bytes = io.BytesIO(img_data)
                     img = PIL.Image.open(img_bytes)
-            else:
-                # If there is no valid product_img_url field, use the default or placeholder image instead
-                st.error(f"No valid image URL found for {item['product_title']}")
-                img_data = requests.get(default_image_url).content
-                img_bytes = io.BytesIO(img_data)
-                img = PIL.Image.open(img_bytes)
-            # Display the image, title, and similarity score in a column layout
-            col1, col2, col3, col4 = st.columns(4)
-            with col1:
-                st.image(img, width=200)
-                #st.image(f'<img src="{img}" style="border-radius: 50%; width:200">',unsafe_allow_html=True)
-            with col2:
-                st.markdown(f"- Title - {item['product_title']}")
-                st.markdown(f"- Rating - {item['rating']}")
-                st.markdown(f"- Similarity - {item['similarity']}")
-            #with col3:
-                            
-        st.success('Successful Recommendation! Have a Nice Day', icon="✅")
-               
+                # Display the image, title, and similarity score in a column layout
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.image(img, width=150, use_column_width ="auto")
+                with col2:
+                    st.markdown(f"- Product ID - {item['product_id']}")
+                    st.markdown(f"- Title - {item['product_title']}")
+                    st.markdown(f"- Brand - {item['brand_name']}")
+                    st.markdown(f"- Rating - {item['rating']}")
+                    st.markdown(f"- Category - {item['category']}") 
+                    st.markdown(f"- Review - {item['review_text'].replace('$', 'USD')}")
+                    st.markdown(f"- Similarity - {item['similarity']}")          
+            st.success('**Successful Recommendation!**', icon="✅")
+else:
+    if not brand_name and category or brand_name and not category:
+        st.warning("Please Choose a value from Brand & Category")              
